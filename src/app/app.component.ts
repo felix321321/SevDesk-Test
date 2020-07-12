9faeb250e7d5a112066af53a1ccc632cf9b46c28
felix321321 <56563557+felix321321@users.noreply.github.com>
2020-07-12 18:27:02 +0200

    Add files via upload
    3.
    Bitcoin API hinzugefügt (kleine Abweichung zur Originalaufgabe, da ich erst einen API Key anfordern müsste, Screenshots included)
    verwendete API "https://www.coindesk.com/coindesk-api"
    Reload Button hinzugefügt
    Modular Neuerstellung von Preisinfos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  prices = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    let observable = this.http.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+    observable.subscribe((response) => {
+
+      for(let key in response.bpi) 
+        if(response.bpi.hasOwnProperty(key))
+          this.prices.push(response.bpi[key]);
+    })
+  }
+
+  reloadButton() {
+    location.reload();
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
