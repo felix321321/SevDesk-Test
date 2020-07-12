1ba4ec024a85a95f02e652b8546508bf971d2867
felix321321 <56563557+felix321321@users.noreply.github.com>
2020-07-12 18:31:33 +0200

    Add files via upload
    Comments - Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,22 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit{
-  
-  prices = [];
+
+  bitcoinData = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     let observable = this.http.get('https://api.coindesk.com/v1/bpi/currentprice.json');
     observable.subscribe((response) => {
-
+      //Daten werden aus JSON file in Array gespeichert
       for(let key in response.bpi) 
         if(response.bpi.hasOwnProperty(key))
-          this.prices.push(response.bpi[key]);
+          this.bitcoinData.push(response.bpi[key]);
     })
   }
 
+  // Lädt Seite neu
   reloadButton() {
     location.reload();
   }
